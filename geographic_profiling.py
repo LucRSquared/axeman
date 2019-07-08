6f903a38be1b35c55c65be4b486891866cb51eef
LucRSquared <LucRSquared@users.noreply.github.com>
2019-07-07 23:52:44 -0500

    learning

--- geographic_profiling.py
+++ geographic_profiling.py
@@ -1,25 +1,38 @@
 import pandas
+import geopandas
 import osmnx as ox
 import networkx as nx
-#import osrm
+import matplotlib.pyplot as plt
+
 
 transport_mode = 'walk'
 
 vics = pandas.read_csv('victims.csv')
 
-lat = vics['Latitude']
-lon = vics['Longitude']
-
 selection = (vics['Confirmed Axeman'] == 'Yes') | (vics['Confirmed Axeman'] == 'Maybe')
 
 vicselect = vics.loc[selection,['Latitude','Longitude']]
-vicselect
+vicselect.head()
 #vicselect.iloc[2,1] # third element, first second column
+lat = vicselect['Latitude']
+lon = vicselect['Longitude']
+
 
 graph = ox.graph_from_place('New-Orleans, LA', network_type=transport_mode, simplify=True)
-edges = ox.graph_to_gdfs(graph, nodes=False, edges=True)
+nodes, edges = ox.graph_to_gdfs(graph, nodes=True, edges=True)
+
+vorig = 0 
+vtarget = 6 
+orig_node = ox.get_nearest_node(graph, (lat[vorig], lon[vorig]))
+target_node = ox.get_nearest_node(graph, (lat[vtarget], lon[vtarget]))
+
+length = nx.shortest_path_length(G=graph, source=orig_node, target=target_node, weight='length')
+
+print(length)
+
+#gdf = geopandas.GeoDataFrame(vicselect, geometry=geopandas.points_from_xy(vicselect['Longitude'], vicselect['Latitude']))
+gdf = geopandas.GeoDataFrame(vicselect, geometry=geopandas.points_from_xy(lon, lat))
+#fig, ax = ox.plot_graph(graph, fig_height=20, fig_width=None, close =False, show =False)
 
 
-#result = osrm.simple_route(
-#                      [21.0566163803209,42.004088575972], [20.9574645547597, 41.5286973392856],
-#                      output='route', overview="full", geometry='wkt')
+#plt.show()
